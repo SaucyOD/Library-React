aa13ad4404fe2bb8605472bfcd38abe9f0be046c
SaucyOD <oarceo45167@gmail.com>
2026-03-01 00:12:39 -0600

    change

---
+++ src/data.js
@@ -0,0 +1,111 @@
+// FAKE DATA
+export const books = [
+  {
+    id: 1,
+    title: "Crack the Coding Interview",
+    url: "https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 49.95,
+    salePrice: 14.95,
+    rating: 4.5,
+    description: "I am not a recruiter. I am a software engineer. And as such, I know what it's like to be asked to whip up brilliant algorithms on the spot and then write flawless code on a whiteboard."
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    url: "https://m.media-amazon.com/images/I/81ANaVZk5LL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 39,
+    salePrice: null,
+    rating: 5,
+    description: "No matter your goals, Atomic Habits offers a proven framework for improving—every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits."
+  },
+  {
+    id: 3,
+    title: "Can't Hurt Me",
+    url: "https://m.media-amazon.com/images/I/81gTRv2HXrL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 29,
+    salePrice: null,
+    rating: 5,
+    description: "For David Goggins, childhood was a nightmare—poverty, prejudice, and physical abuse colored his days and haunted his nights. But through self-discipline, mental toughness, and hard work, Goggins transformed himself."
+  },
+  {
+    id: 4,
+    title: "Deep Work",
+    url: "https://m.media-amazon.com/images/I/91nujEwIpYL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 44,
+    salePrice: 19,
+    rating: 4.5,
+    description: "Deep work is the ability to focus without distraction on a cognitively demanding task. It's a skill that allows you to quickly master complicated information and produce better results in less time."
+  },
+  {
+    id: 5,
+    title: "The 10X Rule",
+    url: "https://m.media-amazon.com/images/I/71hjq42hvGL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 32,
+    salePrice: null,
+    rating: 5,
+    description: "Achieve 'Massive Action' results and accomplish your business dreams! While most people operate with only three degrees of action, if you're after big goals, you don't want to settle for the ordinary."
+  },
+  {
+    id: 6,
+    title: "Sell Or Be Sold",
+    url: "https://m.media-amazon.com/images/I/616OvChM3RL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 70,
+    salePrice: 12.5,
+    rating: 5,
+    description: "Whether it's selling your company's product in the boardroom or selling yourself on eating healthy, everything in life can and should be treated as a sale."
+  },
+  {
+    id: 7,
+    title: "Rich Dad Poor Dad",
+    url: "https://m.media-amazon.com/images/I/81bsw6fnUiL.jpg",
+    originalPrice: 11,
+    salePrice: 10,
+    rating: 4,
+    description: "Rich Dad Poor Dad is Robert's story of growing up with two dads—his real father and the father of his best friend, his rich dad—and the ways in which both men shaped his thoughts about money and investing."
+  },
+  {
+    id: 8,
+    title: "Cashflow Quadrant",
+    url: "https://m.media-amazon.com/images/I/71+SWQ6xj1L._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 38,
+    salePrice: 17.95,
+    rating: 4.5,
+    description: "Rich Dad's CASHFLOW Quadrant is a guide to financial freedom. It reveals how some people work less, earn more, pay less in taxes, and learn to become financially free."
+  },
+  {
+    id: 9,
+    title: "48 Laws of Power Summary",
+    url: "https://m.media-amazon.com/images/I/611X8GI7hpL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 35,
+    salePrice: 19.95,
+    rating: 4.5,
+    description: "Amoral, cunning, ruthless, and instructive, this multi-million-copy New York Times bestseller is the definitive manual for anyone interested in gaining, observing, or defending against ultimate control."
+  },
+  {
+    id: 10,
+    title: "The 5 Second Rule",
+    url: "https://m.media-amazon.com/images/I/91wOERyhh7L._UF1000,1000_QL80_.jpg",
+    originalPrice: 40,
+    salePrice: null,
+    rating: 5,
+    description: "Throughout your life, you've had parents, coaches, teachers, friends, and mentors who have pushed you to be better. What if the secret to having the confidence and courage to enrich your life is simply knowing how to push yourself?"
+  },
+  {
+    id: 11,
+    title: "How to Win Friends & Influence People",
+    url: "https://m.media-amazon.com/images/I/71vK0WVQ4rL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 30,
+    salePrice: 20,
+    rating: 5,
+    description: "You can go after the job you want—and get it! You can take the job you have—and improve it! You can take any situation—and make it work for you!"
+  },
+  {
+    id: 12,
+    title: "Mastery",
+    url: "https://m.media-amazon.com/images/I/51V3121nmkL._AC_UF1000,1000_QL80_.jpg",
+    originalPrice: 30,
+    salePrice: 12.95,
+    rating: 4.5,
+    description: "From the bestselling author of The 48 Laws of Power and The Laws of Human Nature, a vital work revealing that the secret to mastery is already within you."
+  }
+];
